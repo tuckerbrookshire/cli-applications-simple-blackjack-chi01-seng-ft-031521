af22d818088b4a9592acf15c5e32da229de644cd
Tucker Brookshire <tuckerbrookshire@gmail.com>
2021-03-04 19:24:52 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,54 @@
+require "pry"
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  return rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(number)
+  puts "Your cards add up to #{number}" 
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+ gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  card_1 = deal_card
+  card_2 = deal_card
+  total_card = card_1 + card_2
+  display_card_total(total_card)
+  return total_card
 end
 
-def hit?
-  # code hit? here
+def hit?(total_card)
+  prompt_user
+  input = get_user_input
+  if input == 's'
+    total_card
+  elsif input == 'h'
+    new_card = deal_card
+    total_card += new_card
+  else 
+    invalid_command
+    prompt_user
+    get_user_input
+  end
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
 end
 
 #####################################################
@@ -39,6 +56,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  initial_round
+  hit? 
+  binding.pry
+  until total_card >= 21
+  end
 end
     
